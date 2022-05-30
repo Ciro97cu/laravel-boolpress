af6d0a21a90d2dc6c25f96be9bf8b664e5344136
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-30 20:15:04 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
-Auth::routes();
+Auth::routes(); // genera tutti i percorsi alle pagine di login e register
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware("auth")
+    ->namespace("Admin")
+    ->name("admin.")
+    ->prefix("admin")
+    ->group(function () {
+        Route::get("/", "HomeController@index")
+            ->name("home");
+    });
+
+
+Route::get("{any?}", function () {
+    return view("guest.home");
+})->where("any", ".*");

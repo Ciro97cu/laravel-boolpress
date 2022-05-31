2f4262fdbab3f2ef0eabedc5b11f83932ec23fb4
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-31 20:52:46 +0200

    done create + store

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,6 +1,46 @@
-{{-- @extends('layouts.dashboard')
+@extends('layouts.dashboard')
 
 @section('content')
-    ciao
-@endsection --}}
-ciao
+<div class="row justify-content-between">
+    <div class="col-auto">
+        <h1>Creazione nuovo post</h1>
+    </div>
+    <div class="col-auto">
+        <a href="{{ route('admin.posts.index') }}" class="btn btn-primary">Tutti i post</a>
+    </div>
+</div>
+<div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+</div>
+<form action="{{ route("admin.posts.store")}}" method="POST" enctype="multipart/form-data">
+    @csrf
+    <div class="form-group">
+        <label for="title">Titolo</label>
+        <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" placeholder="Inserisci il titolo" value="{{ old("title") }}" required>
+        @error("title")
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="content">Contenuto</label>
+        
+        <textarea name="content" cols="30" rows="10" class="form-control @error('title') is-invalid @enderror" placeholder="Scrivi qui..." required>value="{{ old("content") }}"</textarea>
+        
+        @error("content")
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <button type="submit" class="btn btn-success">Crea post</button>
+    </div>
+</form>
+
+@endsection

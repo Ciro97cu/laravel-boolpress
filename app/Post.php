50373c7b53ed9d8af994c8127871ec0cf50e6d65
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-31 22:07:10 +0200

    added guarded + function

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 
 class Post extends Model
 {
-    protected $fillable = ["title", "content", "slug"];
+    protected $guarded = [];
+
+    public static function generateSlug($title)
+    {
+        $slug = Str::slug($title);
+        $alternativeSlug = $slug;
+        $postFound = Post::where("slug", $slug)->first();
+        $counter = 1;
+        while ($postFound) {
+            $alternativeSlug = $slug . "_" . $counter;
+            $counter++;
+            $postFound = Post::where("slug", $alternativeSlug)->first();
+        }
+        return $alternativeSlug;
+    }
 }

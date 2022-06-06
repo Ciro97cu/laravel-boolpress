846356649e58b806c0bb9672d525ee06166570cd
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-06 15:33:07 +0200

    added guraded, table name and belong to many

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $guarded = [];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "tags";
+
+    public function post()
+    {
+        return $this->belongsToMany("App\Post");
+    }
 }

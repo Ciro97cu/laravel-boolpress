db35df82dd0994e1b80ca7169f7645224a22ffc8
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-09 22:03:33 +0200

    defined mode + routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -7,3 +7,26 @@ Vue.use(VueRouter);
 import HomeComponent from "./pages/HomeComponent"
 import PostsComponent from "./pages/PostsComponent"
 import NotFound from "./pages/NotFound"
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomeComponent
+        },
+        {
+            path: "/posts",
+            name: "posts",
+            component: PostsComponent
+        },
+        {
+            path: "/*",
+            name: "notFound",
+            component: NotFound
+        },
+    ]
+})
+
+export default router;

e6f358b282518e2bccbcbee4aae7d0f310648b5a
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-09 21:59:20 +0200

    created header component with bootstrap navbar

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
+        <router-link class="nav-link" :to="{ name: 'posts' }"
+          >Posts</router-link
+        >
+
+        <a class="nav-link disabled">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style>
+</style>

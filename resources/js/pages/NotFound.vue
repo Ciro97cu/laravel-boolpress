ff49c30dc6363a4f49e9cb463b86644bddc09be0
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-09 21:58:33 +0200

    created not found component

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-12 text-center">
+        <h1>404 - NOT FOUND</h1>
+      </div>
+      <div class="col-12 text-center">
+        <router-link :to="{ name: 'home' }">Ritorna alla Home</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style>
+</style>

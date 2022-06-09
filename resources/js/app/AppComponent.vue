b998bb4ae83ea3fc831a2841562d915fe9a2e31f
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-09 22:00:04 +0200

    imported globally header + added router-view

--- resources/js/app/AppComponent.vue
+++ resources/js/app/AppComponent.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>WIP</div>
+  <div>
+    <HeaderComponent />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import HeaderComponent from "../components/HeaderComponent.vue";
+
 export default {
   name: "AppComponent",
+  components: {
+    HeaderComponent,
+  },
 };
 </script>
 

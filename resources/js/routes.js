fc75f0470a854ee57bb28a4c68920c1a45e1ceb6
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-09 22:03:07 +0200

    imported route component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,3 +3,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
+
+import HomeComponent from "./pages/HomeComponent"
+import PostsComponent from "./pages/PostsComponent"
+import NotFound from "./pages/NotFound"

f431a3e00cc33e26b3e9da30d53000dc501f0297
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-10 20:54:16 +0200

    fixed image card size + added detail button

--- resources/js/components/PostCard.vue
+++ resources/js/components/PostCard.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="row justify-content-center">
-    <!-- <div class="col-4 bg-success" v-for="post in posts" :key="post.id">
-      {{ post.title }}
-    </div> -->
     <div
       class="card m-3"
       style="width: 18rem"
       v-for="post in posts"
       :key="post.id"
     >
-      <img
-        :src="'storage/' + post.cover"
-        class="card-img-top"
-        :alt="post.title"
-      />
+      <div class="wrapper_img">
+        <img :src="'storage/' + post.cover" class="h-100" :alt="post.title" />
+      </div>
       <div class="card-body">
         <h5 class="card-title">{{ post.title }}</h5>
         <p class="card-text">
           {{ post.content }}
         </p>
+        <router-link
+          :to="{ name: 'details', params: { id: post.id } }"
+          class="btn btn-primary"
+          >Dettaglio</router-link
+        >
       </div>
     </div>
   </div>
@@ -36,5 +36,14 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.wrapper_img {
+  max-height: 160px;
+
+  img {
+    object-fit: cover;
+    object-position: top;
+    width: 100%;
+  }
+}
 </style>

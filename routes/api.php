0625ecd22faacc1c5547b7b84bcb33d00594e041
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-10 20:52:17 +0200

    added show route

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/posts", "Api\PostController@index")->name("index");
+Route::get("/posts", "Api\PostController@index")->name("posts.index");
+Route::get("/posts/{id}", "Api\PostController@show")->name("posts.show");

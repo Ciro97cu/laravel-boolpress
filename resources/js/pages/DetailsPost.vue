cdd542dc68409372715944261ad5e3f0cecab230
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-10 20:52:41 +0200

    created details component + added basic layout and axios call

---
+++ resources/js/pages/DetailsPost.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row justify-content-center">
+    <div v-if="detail">
+      {{ detail.title }}
+    </div>
+    <div v-else>Caricamento in corso</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailsPost",
+  data() {
+    return {
+      detail: undefined,
+    };
+  },
+  mounted() {
+    const id = this.$route.params.id;
+    window.axios
+      .get("/api/posts/" + id)
+      .then((results) => {
+        this.detail = results.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+};
+</script>
+
+<style>
+</style>

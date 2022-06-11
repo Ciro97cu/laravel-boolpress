a1ae2ad551fba8eb9123a43e5d959f082cc91626
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-06-11 14:35:02 +0200

    added pagination

--- resources/js/pages/PostsComponent.vue
+++ resources/js/pages/PostsComponent.vue
@@ -2,6 +2,25 @@
   <div class="container">
     <div v-if="arrayPost.length > 0">
       <PostCard :posts="arrayPost" />
+      <div class="row justify-content-center">
+        <button
+          class="btn btn-primary"
+          @click="goPreviousPage()"
+          :disabled="!previousPage"
+        >
+          Prev
+        </button>
+        <span class="p-2 mx-2 text-white rounded bg-primary"
+          >{{ currentPage }} - {{ lastPage }}</span
+        >
+        <button
+          class="btn btn-primary"
+          @click="goNextPage()"
+          :disabled="!nextPage"
+        >
+          Next
+        </button>
+      </div>
     </div>
     <div v-else>Caricamento in corso</div>
   </div>
@@ -17,17 +36,37 @@ export default {
   data() {
     return {
       arrayPost: [],
+      currentPage: 1,
+      lastPage: 1,
+      previousPage: "",
+      nextPage: "",
     };
   },
+  methods: {
+    loadPage(url) {
+      window.axios
+        .get(url)
+        .then((results) => {
+          this.arrayPost = results.data.data;
+          this.currentPage = results.data.current_page;
+          this.lastPage = results.data.last_page;
+          this.previousPage = results.data.prev_page_url;
+          this.nextPage = results.data.next_page_url;
+          console.log(results);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    goPreviousPage() {
+      this.loadPage(this.previousPage);
+    },
+    goNextPage() {
+      this.loadPage(this.nextPage);
+    },
+  },
   mounted() {
-    window.axios
-      .get("/api/posts")
-      .then((results) => {
-        this.arrayPost = results.data;
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    this.loadPage("/api/posts");
   },
 };
 </script>
